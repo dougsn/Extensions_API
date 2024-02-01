bef5727981c34851ca4662a704660abef18d8cd0
dnascimento <douglassnasciment@gmail.com>
2024-02-01 15:16:27 -0300

    fix: AllowedOrigins (CORS)

--- backend/src/main/java/com/extensions/infra/config/WebConfig.java
+++ backend/src/main/java/com/extensions/infra/config/WebConfig.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${cors.originPatterns}")
-    private String corsOriginPatterns = "";
+    @Value("${cors.originPatterns:*}")
+    private String corsOriginPatterns;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
