4f180aed9f4d4c1491e18930d3351f36452c2173
dnascimento <douglassnasciment@gmail.com>
2024-02-01 16:31:14 -0300

    deploy: Trocando ip da api.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-   baseURL: "http://localhost:8080/api" // IP do local
-   // baseURL: "http://192.168.0.177:8080/api" // IP do servidor, trocar quando for realizar o build. (Ubuntu)
+   // baseURL: "http://localhost:8080/api" // IP do local
+   baseURL: "https://extension-api.up.railway.app/api" // IP do servidor, trocar quando for realizar o build. (Ubuntu)
 })
